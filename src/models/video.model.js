f3efca196fde57783aca79ff866e7f5dfcbea211
lavishabhatia <lavishabhatia1012@gmail.com>
2024-04-24 17:38:28 +0530

    add user video model

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      requied: true,
+    },
+    description: {
+      type: String,
+      requied: true,
+    },
+    duration: {
+      type: Number, //cloudinary
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublised: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.TYpes.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema);
