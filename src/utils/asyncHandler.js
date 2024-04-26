886f7e91100985012936f882832a5d19c30b98ca
lavishabhatia <lavishabhatia1012@gmail.com>
2024-04-26 15:51:52 +0530

    user register

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,8 @@
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
+    console.log("hii");
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
-
 };
 
 export { asyncHandler };
-
-
